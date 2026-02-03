d581e4ee23ea2ca8b6ca45b8853b2d093ef67019
Robert MacWha <github@macwha.com>
2026-02-02 23:42:40 -0500

    revert to shell

---
+++ shell.nix
@@ -0,0 +1,28 @@
+let
+  pkgs =
+    import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-25.11.tar.gz")
+      {
+        overlays = [
+          (import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))
+        ];
+      };
+  unstable =
+    import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz")
+      { };
+
+  rustToolchain = pkgs.rust-bin.stable."1.87.0".default.override {
+    extensions = [ "rust-src" ];
+  };
+
+  circom = pkgs.callPackage ./flakes/circom.nix { };
+in
+pkgs.mkShell {
+  packages = [
+    rustToolchain
+    pkgs.rust-analyzer
+    circom
+    pkgs.just
+
+    unstable.foundry
+  ];
+}
