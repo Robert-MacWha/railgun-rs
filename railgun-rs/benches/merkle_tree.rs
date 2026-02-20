989abfda129d17ca7bd9c88d5224ac36b4903448
Robert MacWha <github@macwha.com>
2026-02-20 10:06:10 -0500

    tidy bench

--- railgun-rs/benches/merkle_tree.rs
+++ railgun-rs/benches/merkle_tree.rs
@@ -6,7 +6,7 @@ const TREE_DEPTH: usize = 14;
 const FULL_TREE_SIZE: usize = 1 << TREE_DEPTH;
 
 fn bench_full_tree_fill(c: &mut Criterion) {
-    c.bench_function("fill_full_16depth_tree", |b| {
+    c.bench_function("full_tree_fill", |b| {
         b.iter(|| {
             let mut tree = MerkleTree::new(0);
             let leaves: Vec<U256> = (1..=FULL_TREE_SIZE as u64).map(U256::from).collect();
@@ -25,7 +25,7 @@ fn bench_single_leaf_edit(c: &mut Criterion) {
         batch.insert_leaves(&leaves, 0);
     }
 
-    c.bench_function("single_leaf_edit_16depth_tree", |b| {
+    c.bench_function("single_leaf_edit", |b| {
         b.iter(|| {
             tree.insert_leaf(U256::from(42u64), 0);
         });
